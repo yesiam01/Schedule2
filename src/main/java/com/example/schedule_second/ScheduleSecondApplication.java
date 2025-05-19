4ad03ecaab98f46cbb6fa2f93fe2dd85ae73dc1d
Hyeon woo <renzpark@naver.com>
2025-05-19 23:18:17 +0900

    0단계 완료

---
+++ src/main/java/com/example/schedule_second/ScheduleSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedule_second/ScheduleSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule_second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
